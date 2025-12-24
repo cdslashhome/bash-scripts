4dde2d9420ae6cc0bb2cecaa4228276b81a3cfb1
slashhome <nazayuza@gmail.com>
2025-12-24 12:34:58 -0600

    Update ord.sh

--- Ord/ord.sh
+++ Ord/ord.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-cd ~/Descargas
+cd ~/Descargas #Ruta de tu carpeta de descargas
 
 leer(){ 
 	ls -A | wc -l
@@ -9,7 +9,7 @@ a=$(leer)
 
 move(){
 
-mv *.png *.jpg *.jpeg *.gif *.webp ~/Imágenes 2>/dev/null
+mv *.png *.jpg *.jpeg *.gif *.webp ~/Imágenes 2>/dev/null #inserte su ruta propia de aki pa abajo
 mv *.mp4 *.mov *.mkv ~/Vídeos 2>/dev/null
 mv *.pdf ~/Documentos 2>/dev/null
 mv *.zip *.tgz *.tar.gz *.rar ~/comprimidos 2>/dev/null
@@ -18,19 +18,17 @@ mv *.mp3 /home/irachan/Música/Musica/FormatoMP3 2>/dev/null
 mv *.ISO /home/irachan/test_share/SO 2>/dev/null
 
 }
-if [ "$(leer)" -gt 50 ]; then
-mkdir -p ~/Imágenes
-mkdir -p ~/Vídeos
-mkdir -p ~/Documentos
-mkdir -p ~/comprimidos
-mkdir -p /home/irachan/Música/Musica/FormatoMP3
-mkdir -p /home/irachan/test_share/SO
+if [ "$(leer)" -gt 50 ]; then #Puedes cambiar la cantidad necesaria de archivos para que se ordene la carpeta cambiando el "50" por el numero que desees
+mkdir -p ~/Imágenes #cambia a tu ruta
+mkdir -p ~/Vídeos #cambia a tu ruta
+mkdir -p ~/Documentos #cambia a tu ruta
+mkdir -p ~/comprimidos #cambia a tu ruta
+mkdir -p /home/irachan/Música/Musica/FormatoMP3 #cambia a tu ruta
+mkdir -p /home/irachan/test_share/SO #Cambia tu ruta
 move
-echo movido exitosamente
 notify-send "Orden completada" "Se movieron $a archivos"
 
 else 
 notify-send "No se completo la orden" "Archivos insuficientes en el directorio. (necesarios: <50)"
-echo hizo nada
 
 fi
